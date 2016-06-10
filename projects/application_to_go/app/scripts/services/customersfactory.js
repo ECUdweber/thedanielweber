11191f9110a2ba228ac1e82438338acd5952a11c
Daniel Weber <daniel@bitlathe.com>
2016-06-10 15:59:55 -0400

    Created customers service

--- projects/application_to_go/app/scripts/services/customersfactory.js
+++ projects/application_to_go/app/scripts/services/customersfactory.js
@@ -9,7 +9,33 @@
  */
 
 var customers_route = "/api/customers/";
+var FIREBASE_URL = "https://app-to-go.firebaseio.com";
 
+angular.module('applicationToGoApp')
+	.factory('customersFactory', function($firebaseArray) {
+	    var customers = $firebaseArray(new Firebase(FIREBASE_URL));
+
+	    var Customer = {
+	        all: function () {
+	          return customers;
+	        },
+	        create: function (item) {
+	          return customers.$add(item);
+	        },
+	        get: function (itemId) {
+	          return $firebase(ref.child('items').child(itemId)).$asObject();
+	        },
+	        update: function (itemId, item) {
+	          return $firebase(ref.child('items').child(itemId)).update(item);
+	        },
+	        delete: function (item) {
+	          return customers.$remove(item);
+	        }
+	    };
+	    return Customer;
+	});
+
+/*
 angular.module('applicationToGoApp', ["firebase"])
   .factory('customersFactory', function customersFactory($resource, $firebaseObject) {
     return $resource(customers_route + ':id',
@@ -17,3 +43,4 @@ angular.module('applicationToGoApp', ["firebase"])
       {'update': {method:'PUT'}}
     );
   }); 
+*/
